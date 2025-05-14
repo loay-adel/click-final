949c6e8fc4f5adf11efd5af15be279c784ae83b5
loay <161643055+loay-adel@users.noreply.github.com>
2025-05-14 16:07:38 +0300

    fixed the unauthorized when delteing user

--- middlewares/authorize.middleware.js
+++ middlewares/authorize.middleware.js
@@ -1,11 +1,12 @@
 const authorize = (...allowedRoles) => {
   return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
+    if (!req.user || allowedRoles.includes(req.user.role)) {
       return res.status(403).json({
         status: 403,
         data: { data: null, message: "Access Denied: Unauthorized Role" },
       });
     }
+
     next();
   };
 };
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -16,6 +16,7 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
+
     next();
   } catch (err) {
     return res.status(403).json({
