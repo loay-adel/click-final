d6f0d061c109df26d9f1ba79d4d10709c21fdcd5
mostafa mahmoud <mostafa.mahmoudegy10@gmail.com>
2025-05-16 00:30:39 +0300

    a

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const connectDB = async () => {
 };
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:3000", "https://gioco-rx7d.vercel.app",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true,
 };
